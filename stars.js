fdf85322cee76dc05ece540ede5634598c3dd3e3
malveiracaiodev <caiomelo159@gmail.com>
2025-10-04 14:40:53 -0300

    correção do fundo

--- stars.js
+++ stars.js
@@ -8,15 +8,13 @@ for (let i = 0; i < totalStars; i++) {
   // Posição horizontal aleatória
   star.style.left = `${Math.random() * 100}vw`;
 
-  // Tamanho aleatório entre 1px e 3px
+  // Tamanho aleatório
   const size = Math.random() * 2 + 1;
   star.style.width = `${size}px`;
   star.style.height = `${size}px`;
 
-  // Opacidade aleatória
+  // Opacidade e animação
   star.style.opacity = Math.random() * 0.8 + 0.2;
-
-  // Duração e delay da animação
   star.style.animationDuration = `${6 + Math.random() * 4}s`;
   star.style.animationDelay = `${Math.random() * 5}s`;
 
